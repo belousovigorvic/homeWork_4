493b5b5b4b79f0b6f5687011f59175eba049066a
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-04-17 14:49:37 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var list =  [null, 123, undefined, null, "qwerty", 34, undefined, null, null, 43, "aerg", 'пкцйк', null, 'uioo'];
+
+var typeNull = [];
+var typeString = [];
+var typeNumber = [];
+var typeUndefined = [];
+
+for (var i = 0; i < list.length; i++) {
+    if (list[i] === null) {
+        typeNull.push(list[i]);
+    }
+    if (typeof list[i] === "string") {
+        typeString.push(list[i]);
+    }
+    if (typeof list[i] === "number") {
+        typeNumber.push(list[i]);
+    }
+    if (typeof list[i] === 'undefined') {
+        typeUndefined.push(list[i]);
+    }
+}
+
+var typeData = []
+typeData.push(typeNull, typeString, typeNumber, typeUndefined)
+
+typeData.sort((a, b) => b.length - a.length)
+
+
+console.log(typeNull);
+console.log(typeString);
+console.log(typeNumber);
+console.log(typeUndefined);
+
+console.log(typeData) // Отсортированные массивы
+
+
+
